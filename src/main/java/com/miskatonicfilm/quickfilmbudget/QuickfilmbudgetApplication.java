9fff468746a1935f26d2bac87fb918fda0619555
blarg <blarg@blargs-MacBook-Pro.local>
2022-04-06 09:26:51 -0600

    Initial commit

---
+++ src/main/java/com/miskatonicfilm/quickfilmbudget/QuickfilmbudgetApplication.java
@@ -0,0 +1,13 @@
+package com.miskatonicfilm.quickfilmbudget;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickfilmbudgetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickfilmbudgetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/miskatonicfilm/quickfilmbudget/QuickfilmbudgetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miskatonicfilm.quickfilmbudget;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickfilmbudgetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
